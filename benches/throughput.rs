ec0bbcbe02fb4a49f36dd3d476d0432ae5c9d582
John Turner <jturner.usa@gmail.com>
2024-12-27 18:55:30 -0500

    include xorshift and pcg in benchmarks

--- benches/throughput.rs
+++ benches/throughput.rs
@@ -4,6 +4,9 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand_core::{RngCore, SeedableRng};
 use rand_murmur3::{Mixer32, Mixer64};
 
+const SEED: u64 = 10;
+const BUFFER_SIZE: usize = 4000000;
+
 #[derive(Clone, Copy, Debug)]
 struct NoopRng;
 
@@ -61,36 +64,62 @@ fn throughput<R: RngCore>(mut rng: R, size: usize) {
 }
 
 fn noop(c: &mut Criterion) {
-    let rng = NoopRng;
-
     c.bench_function("noop rng", |bench| {
-        bench.iter(|| throughput::<NoopRng>(black_box(rng), black_box(4000000)))
+        bench.iter(|| throughput::<NoopRng>(black_box(NoopRng), black_box(BUFFER_SIZE)))
     });
 }
 
 fn counter(c: &mut Criterion) {
-    let rng = CountingRng { state: 0 };
-
     c.bench_function("counting rng", |bench| {
-        bench.iter(|| throughput::<CountingRng>(black_box(rng), black_box(4000000)))
+        bench.iter(|| {
+            throughput::<CountingRng>(black_box(CountingRng { state: 0 }), black_box(BUFFER_SIZE))
+        })
     });
 }
 
 fn mixer32(c: &mut Criterion) {
-    let rng = Mixer32::from_seed(10u32.to_ne_bytes());
-
     c.bench_function("mixer32", |bench| {
-        bench.iter(|| throughput::<Mixer32>(black_box(rng), black_box(4000000)))
+        bench.iter(|| {
+            throughput::<Mixer32>(
+                black_box(Mixer32::from_seed((SEED as u32).to_ne_bytes())),
+                black_box(BUFFER_SIZE),
+            )
+        })
     });
 }
 
 fn mixer64(c: &mut Criterion) {
-    let rng = Mixer64::from_seed(10u64.to_ne_bytes());
-
-    c.bench_function("mixer32", |bench| {
-        bench.iter(|| throughput::<Mixer64>(black_box(rng), black_box(4000000)))
+    c.bench_function("mixer64", |bench| {
+        bench.iter(|| {
+            throughput::<Mixer64>(
+                black_box(Mixer64::from_seed(10u64.to_ne_bytes())),
+                black_box(BUFFER_SIZE),
+            )
+        })
     });
 }
 
-criterion_group!(benches, noop, counter, mixer32, mixer64);
+fn xorshift(c: &mut Criterion) {
+    c.bench_function("xorshift64", |bench| {
+        bench.iter(|| {
+            throughput(
+                black_box(rand_xorshift::XorShiftRng::seed_from_u64(SEED)),
+                black_box(BUFFER_SIZE),
+            )
+        })
+    });
+}
+
+fn pcg(c: &mut Criterion) {
+    c.bench_function("pcg", |bench| {
+        bench.iter(|| {
+            throughput(
+                black_box(rand_pcg::Pcg64::seed_from_u64(SEED)),
+                black_box(BUFFER_SIZE),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, noop, counter, mixer32, mixer64, xorshift, pcg);
 criterion_main!(benches);
